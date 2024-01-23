2347447dc85e09aea29323e17e34798a6b477390
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2024-01-23 15:05:54 +0700

    PokeSearch Complete Tasks

--- pokemon-search/src/app/app.component.ts
+++ pokemon-search/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,42 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pokemon-search';
+  input_text: string = '';
+  pokemon_list: any[] = [];
+  pokemon_filtered: any[] = [];
+
+  searchOn(text: string) {
+    text ? this.input_text = text : this.input_text = ''
+  }
+
+  fetchPokemonData() {
+    const promises: Promise<any>[] = [];
+    for (let i = 1; i <= 150; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+      promises.push(fetch(url).then((res) => res.json()));
+    }
+
+    Promise.all(promises)
+      .then((pokemonData) => {
+        this.pokemon_list = pokemonData;
+        this.pokemon_filtered = this.pokemon_list;
+      })
+      .catch((error) => {
+        console.error('Error fetching Pokemon data:', error);
+      });
+  }
+
+  filterPokemon(text: string) {
+    if (!text) {
+      return this.pokemon_filtered = this.pokemon_list;
+    }
+    const lowercaseText = text.toLowerCase();
+    return this.pokemon_filtered = this.pokemon_list.filter(pokemon => pokemon.name.toLowerCase().includes(lowercaseText));
+  }
+
+  ngOnInit(): void {
+    this.fetchPokemonData()
+  }
 }
