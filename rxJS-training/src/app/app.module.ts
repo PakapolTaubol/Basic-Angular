ea0143bd699cf1088bfd43af58c2d48930fa8039
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2024-02-01 13:32:36 +0700

    [RxJS] Init Router Plant

--- rxJS-training/src/app/app.module.ts
+++ rxJS-training/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PlantComponent } from './plant/plant.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    PlantComponent,
+  ],
   imports: [
     CommonModule,
+    RouterModule,
+    RouterOutlet,
     AppRoutingModule
-  ]
+  ],
+  exports: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- rxJS-training/src/app/app.routes.ts
+++ rxJS-training/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PlantComponent } from './plant/plant.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: PlantComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full',
+      },
+];
