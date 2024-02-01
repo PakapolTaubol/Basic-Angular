176096bc4b31b953d5a1dc5cd1090677b795a407
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2024-02-01 11:06:30 +0700

    [RxJS] init Plant

--- rxJS-training/src/app/app-routing/app-routing-routing.module.ts
+++ rxJS-training/src/app/app-routing/app-routing-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlantComponent } from '../plant/plant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PlantComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
